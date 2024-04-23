bdf894c7c4845d3fbbc2b84037fcf2943e8f6b10
joaovitorpd <joaovitorpd@gmail.com>
2024-04-23 13:15:52 -0300

    Class 6 - GetIt implemented

--- lib/home.dart
+++ lib/home.dart
@@ -2,14 +2,14 @@ import 'package:counter_mobx/body.dart';
 import 'package:counter_mobx/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:provider/provider.dart';
+import 'package:get_it/get_it.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = Provider.of<Controller>(context);
+    final controller = GetIt.I.get<Controller>();
 
     return Scaffold(
       appBar: AppBar(
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:counter_mobx/controller.dart';
 import 'package:counter_mobx/home.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:get_it/get_it.dart';
 
 void main() {
+  GetIt getIt = GetIt.I;
+  getIt.registerSingleton<Controller>(Controller());
+
   runApp(const MyApp());
 }
 
@@ -12,21 +15,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<Controller>(
-          create: (_) => Controller(),
-          dispose: (_, controler) => controler.dispose(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const MyHomePage(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: const MyHomePage(),
     );
   }
 }
